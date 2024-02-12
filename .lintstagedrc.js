59bbf492f057fb132ad66e37be4d82b33d778188
Aler Denisov <aler.zampillo@gmail.com>
2024-02-12 08:00:52 +0300

    feat: nx-container

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     '{apps,libs}/**/*.{ts,js,html,json,scss,css,md}': [
-        'nx affected:lint --uncommitted --fix true',
+        'nx affected -t lint --parallel  --base=main --head=HEAD',
     ],
     '*.{ts,js,html,json,scss,css,md,yaml,yml}': [
         'nx format:write --base=main --head=HEAD',

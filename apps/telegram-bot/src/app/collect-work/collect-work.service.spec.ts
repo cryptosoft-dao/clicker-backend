0110f3243ae9627be7294d296de3198264de272f
Aler Denisov <aler.zampillo@gmail.com>
2024-02-12 08:29:16 +0300

    fix: foo specs

--- apps/telegram-bot/src/app/collect-work/collect-work.service.spec.ts
+++ apps/telegram-bot/src/app/collect-work/collect-work.service.spec.ts
@@ -1,18 +1,3 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { CollectWorkService } from './collect-work.service';
-
-// describe('CollectWorkService', () => {
-//     let service: CollectWorkService;
-
-//     beforeEach(async () => {
-//         const module: TestingModule = await Test.createTestingModule({
-//             providers: [CollectWorkService],
-//         }).compile();
-
-//         service = module.get<CollectWorkService>(CollectWorkService);
-//     });
-
-//     it('should be defined', () => {
-//         expect(service).toBeDefined();
-//     });
-// });
+describe('boom', () => {
+    it('not implemented yet', async () => {});
+});
